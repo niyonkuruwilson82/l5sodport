03f7a8e7ac92fe0572e9f271f8b08c1469c3dd9c
NIWILSON <niyonkuruwilson82@gmail.com>
2025-04-24 14:08:35 +0200

    second commit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
       <footer className="bg-gray-800 text-white text-center py-4 mt-auto">
-        <p>&copy; {new Date().getFullYear()} NIELLY TECH Web Development Company. All rights reserved.</p>
+        <p>&copy; {new Date().getFullYear()} L5 SOD TECH Web Development Company |EAV KIVUMU TSS 20225. All rights reserved.</p>
       </footer>
     );
   }
